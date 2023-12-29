7952b1f48e1818eb3eece2f5cf59c2676df98b88
Christian Kirkegaard <christian@lowpoly.dk>
2023-12-29 19:41:45 +0100

    FPS example

--- example/src/App.js
+++ example/src/App.js
@@ -3,6 +3,8 @@ import { useCanvas } from "react-use-canvas";
 import "./App.css";
 
 function Example01() {
+  const [fps, setFPS] = useState(60);
+
   const setup = ({ context, height, width }) => {
     context.translate(width / 2, height / 2);
   };
@@ -19,9 +21,31 @@ function Example01() {
     context.fill();
   };
 
-  const { ref } = useCanvas({ setup, draw });
+  const { ref } = useCanvas({ setup, draw, options: { fps } });
 
-  return <canvas ref={ref} />;
+  return (
+    <div>
+      <div>
+        <label htmlFor="ex01fps">FPS </label>
+        <select
+          id="ex01fps"
+          type="checkbox"
+          defaultValue={60}
+          onChange={(e) => {
+            setFPS(parseInt(e.target.value));
+          }}
+        >
+          <option value="5">5</option>
+          <option value="15">15</option>
+          <option value="30">30</option>
+          <option value="60">60</option>
+          <option value="120">120</option>
+          <option value="240">240</option>
+        </select>
+      </div>
+      <canvas ref={ref} />
+    </div>
+  );
 }
 
 function Example02() {
@@ -61,13 +85,49 @@ function Example02() {
     <div>
       <div>
         <input
-          id="pause"
+          id="ex02pause"
           type="checkbox"
           onClick={() => {
             setIsPlaying(!isPlaying);
           }}
         />
-        <label htmlFor="pause">Pause?</label>
+        <label htmlFor="ex02pause">Pause?</label>
+      </div>
+      <canvas ref={ref} />
+    </div>
+  );
+}
+
+function Example03() {
+  const [isPlaying, setIsPlaying] = useState(true);
+
+  const draw = ({ context, time, height, width }) => {
+    context.clearRect(0, 0, width, height);
+    context.font = "14px sans-serif";
+    context.fillStyle = "white";
+    context.fillText(`Time count: ${time}`, 0, height / 2);
+  };
+
+  const { ref } = useCanvas({
+    draw,
+    options: {
+      pause: !isPlaying,
+      height: 50,
+      width: 250,
+    },
+  });
+
+  return (
+    <div>
+      <div>
+        <input
+          id="ex03pause"
+          type="checkbox"
+          onClick={() => {
+            setIsPlaying(!isPlaying);
+          }}
+        />
+        <label htmlFor="ex03pause">Pause?</label>
       </div>
       <canvas ref={ref} />
     </div>
@@ -86,6 +146,8 @@ function App() {
       <Example01 />
       <h2>Example02</h2>
       <Example02 />
+      <h2>Example03</h2>
+      <Example03 />
     </main>
   );
 }
