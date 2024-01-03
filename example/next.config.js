c51ee6cd04c7af7d4dea0000767bb93901423f1a
Christian Kirkegaard <christian@lowpoly.dk>
2024-01-04 00:36:20 +0100

    Set basepath

--- example/next.config.js
+++ example/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export"
+  output: "export",
+  basePath: "react-use-canvas",
+  reactStrictMode: true,
+  swcMinify: true
 }
 
 module.exports = nextConfig
