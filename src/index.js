a4c430f1c3b8d40c045fe74c7a122df4f9587ead
Christian Kirkegaard <christian@lowpoly.dk>
2024-06-18 22:36:12 +0200

    Add missing exports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export { useCanvas } from "./hook";
 export { Vec2 } from "./math/Vec2";
 export { Vec3 } from "./math/Vec3";
-export * from "./math/Random";
+export { getRandomFloatBetween, getRandomIntegerBetween } from "./math/Random";
